a7beacabc669e8da935dc810719fbfbe5e12be32
Benjamin Danjoux <benjamin.danjoux@gmail.com>
2019-03-17 17:21:37 +0100

    Added necessary artifact to tomcat server

--- src/org/tutorial/HelloWorldServlet.java
+++ src/org/tutorial/HelloWorldServlet.java
@@ -2,13 +2,13 @@ package org.tutorial;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
-
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet( name="HelloWorld", urlPatterns = "/HelloWorld" )
 public class HelloWorldServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
