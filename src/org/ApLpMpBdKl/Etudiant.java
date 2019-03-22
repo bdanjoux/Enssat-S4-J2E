05110796abfebe0e91e88c8e06e070acfc5f3c90
Benjamin Danjoux <benjamin.danjoux@gmail.com>
2019-03-22 12:07:55 +0100

    Merge branch 'master' of https://github.com/bdanjoux/Enssat-S4-J2E into ClassicJavaDev
    # Conflicts:
    #	.idea/vcs.xml

--- src/org/ApLpMpBdKl/Etudiant.java
+++ src/org/ApLpMpBdKl/Etudiant.java
@@ -8,6 +8,7 @@ enum MentionBac {AssezBien,Bien,TresBien}
 enum Diplome {Brevet,Bac,L1,L2,L3,M1,M2}
 
 public class Etudiant {
+
     private int id;
     private String nom;
     private String prenom;
@@ -20,6 +21,104 @@ public class Etudiant {
     private Diplome diplome;
     private Date dateDiplome;
     private String villeDiplome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public URL getCourrielPro() {
+        return courrielPro;
+    }
+
+    public void setCourrielPro(URL courrielPro) {
+        this.courrielPro = courrielPro;
+    }
+
+    public URL getCourrielPerso() {
+        return courrielPerso;
+    }
+
+    public void setCourrielPerso(URL courrielPerso) {
+        this.courrielPerso = courrielPerso;
+    }
+
+    public SerieBac getSerieBac() {
+        return serieBac;
+    }
+
+    public void setSerieBac(SerieBac serieBac) {
+        this.serieBac = serieBac;
+    }
+
+    public Date getDateBac() {
+        return dateBac;
+    }
+
+    public void setDateBac(Date dateBac) {
+        this.dateBac = dateBac;
+    }
+
+    public MentionBac getMentionBac() {
+        return mentionBac;
+    }
+
+    public void setMentionBac(MentionBac mentionBac) {
+        this.mentionBac = mentionBac;
+    }
+
+    public Diplome getDiplome() {
+        return diplome;
+    }
+
+    public void setDiplome(Diplome diplome) {
+        this.diplome = diplome;
+    }
+
+    public Date getDateDiplome() {
+        return dateDiplome;
+    }
+
+    public void setDateDiplome(Date dateDiplome) {
+        this.dateDiplome = dateDiplome;
+    }
+
+    public String getVilleDiplome() {
+        return villeDiplome;
+    }
+
+    public void setVilleDiplome(String villeDiplome) {
+        this.villeDiplome = villeDiplome;
+    }
+
+
 
 
 
