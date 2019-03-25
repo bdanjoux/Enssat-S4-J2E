693d3080123a7286c263f3029c062fee5c14e48d
Annaïg <ananaidu29@hotmail.fr>
2019-03-25 11:21:52 +0100

    Ajout d'une classe Authentification pour sécuriser les accès à la BDD utilisateurs

---
+++ src/org/ApLpMpBdKl/Authentification.java
@@ -0,0 +1,12 @@
+package org.ApLpMpBdKl;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Authentification")
+public class Authentification extends HttpServlet {
+    public Authentification(){
+        super();
+    }
+
+}
