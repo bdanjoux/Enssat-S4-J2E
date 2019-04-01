29830d98eb2618e01c02dc4f0096adc670c54bcc
Annaïg <ananaidu29@hotmail.fr>
2019-04-01 17:14:51 +0200

    Ajout de Connexion et Inscription
    Servlet + java + jsp

---
+++ src/org/ApLpMpBdKl/Connexion.java
@@ -0,0 +1,22 @@
+package org.ApLpMpBdKl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Connexion extends HttpServlet{
+    public static final String VUE = "/WEB-INF/Connexion.jsp";
+
+    /* méthode GET qui appelle la page connexion.jsp */
+    public void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Affichage de la page de connexion */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+
+    /* méthode POST */
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+
+    }
+}
